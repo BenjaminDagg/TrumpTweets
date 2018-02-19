7516dfe3ae35e987e614778b39990022a1018900
BenjaminDagg <bdagg@csumb.edu>
2018-02-18 18:26:05 -0800

    - implemented routing to  allow switching between home  and about page via a nav bar
    - Updated CSS to make page look better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,31 @@ import { GhettyImagesComponent } from './ghetty-images/ghetty-images.component';
 import { TweetsComponent } from './tweets/tweets.component';
 import { LinksServiceService } from './links-service.service';
 import { LinkButtonsComponent } from './link-buttons/link-buttons.component';
+import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
+
+
+//routes
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 //put imported modules here
 @NgModule({
   declarations: [
+    HomeComponent,
+    AboutComponent,
     AppComponent,
     GhettyImagesComponent,
     TweetsComponent,
     LinkButtonsComponent,
+    HomeComponent,
   ],
   imports: [
+    RouterModule.forRoot (appRoutes, {enableTracing: true }),
     HttpClientModule,
     BrowserModule
   ],
