3c4bf3bd42c1c2e8a0155dabc4452bfc71a7c26e
BenjaminDagg <bdagg@csumb.edu>
2018-03-06 22:59:34 -0800

    server changes

--- src/main-server.js
+++ src/main-server.js
@@ -7,7 +7,9 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.get('*', function(req,res) {
+	res.sendfile('./src/app/app.component.html');
+})
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080 ,() => console.log('server listening'));
