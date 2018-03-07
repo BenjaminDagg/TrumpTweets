c6ea89b962881c497f72a2e6a12d8016e19cc969
BenjaminDagg <bdagg@csumb.edu>
2018-03-06 23:14:01 -0800

    dist folder

--- main-server.js
+++ main-server.js
@@ -7,9 +7,7 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.get('*', function(req,res) {
-	res.sendfile('./src/app/app.component.html');
-})
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080 ,() => console.log('server listening'));
